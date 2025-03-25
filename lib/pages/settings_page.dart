9933ac2cc6c2fd080640a6409f99cba6a4f711e0
Lucas Silva <lucasvida2005@gmail.com>
2025-03-25 13:59:24 -0300

    finishing up settings page

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_drawer/components/drawer_template.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool isSwitched = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Settings"), centerTitle: true),
+      drawer: DrawerTemplate(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Switch(
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+              activeColor: Colors.blue,
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: isSwitched ? Colors.green : Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
